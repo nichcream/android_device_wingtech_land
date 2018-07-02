28829fbe0535de460ffb2171f4f69794c94156ab
Nich <nctrenco@gmail.com>
2018-07-02 10:17:23 +0800

    land: biometrics: Bypass notifying device about ERROR_CANCELED
    The device would have just returned the same exact message to this
    instance and do nothing else.
    
    Change-Id: I049876c133a7159e865b7448ba55c0077c86ce0a

--- biometrics/BiometricsFingerprint.cpp
+++ biometrics/BiometricsFingerprint.cpp
@@ -184,7 +184,7 @@ Return<RequestStatus> BiometricsFingerprint::cancel() {
         fingerprint_msg_t msg;
         msg.type = FINGERPRINT_ERROR;
         msg.data.error = FINGERPRINT_ERROR_CANCELED;
-        mDevice->notify(&msg);
+        BiometricsFingerprint::getInstance()->notify(&msg);
     }
     return ErrorFilter(ret);
 }
